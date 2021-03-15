e62ea5ad53e663c0b1df0864cfd68b35351d303a
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-03-15 11:42:11 +0600

    Add files via upload

---
+++ EF CORE DZ/EF_CORE_ADO.NET/EF_CORE_ADO.NET/Model/Сlients.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_CORE_ADO.NET.Model
+{
+    public partial class Сlients
+    {
+        public Сlients()
+        {
+            this.BooksTheLibrary = new HashSet<BooksTheLibrary>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Nullable<bool> IsDebtor { get; set; }
+
+        public virtual ICollection<BooksTheLibrary> BooksTheLibrary { get; set; }
+    }
+}
